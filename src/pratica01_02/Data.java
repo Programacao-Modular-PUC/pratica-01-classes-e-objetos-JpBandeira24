63de381236feeda296bb1628b871ede5866575fb
João Pedro Bandeira Ribas <1593655@sga.pucminas.br>
2026-03-16 12:07:09 -0300

    Pratica01_pt2

---
+++ src/pratica01_02/Data.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pratica01_02;
+
+/**
+ *
+ * @author 1593655
+ */
+public class Data {
+    
+}
---
+++ src/pratica01_02/Pratica01_02.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pratica01_02;
+
+/**
+ *
+ * @author 1593655
+ */
+public class Pratica01_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
